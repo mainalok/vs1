7cd0fe27844162ef30c7a11a842e1482c8a9fb62
mainalok <mainalok@skiff.com>
2024-01-30 12:06:24 -0800

    UW
    add proc

---
+++ hw1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Affecting the color - This has been entered by you 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+// Actual code being written 
+Console.WriteLine("Writing stuff to the terminal");
+
+// Returing the color back to normal
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+
+// Another function about writing 
+// Console.write - This will not print on a new line
+Console.Write("Frist");
+
